6e0f0137e4ba8ed17ad8842c4ea742e724a2b8cc
KumundzhievMaxim <kumundzhievmaxim@gmail.com>
2020-12-05 22:00:18 +0100

    Initialised explanatory notebook, data_handler.py.

---
+++ dataset_handler.py
@@ -0,0 +1,15 @@
+# ------------------------------------------
+# 
+# Program created by Maksim Kumundzhiev
+#
+#
+# email: kumundzhievmaxim@gmail.com
+# github: https://github.com/KumundzhievMaxim
+# -------------------------------------------
+
+
+import pandas as pd
+
+dataset_path = './dataset/project_data.csv'
+
+print(pd.read_csv(dataset_path))
